9ce6540a42fe7aeed08807930f33afcef274992b
ravieeeee <pyeonjy97@gmail.com>
2017-11-13 13:00:58 +0900

    add event schema

---
+++ models/events.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose'),
+    mongoosePaginate = require('mongoose-paginate'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+  title: {type: String, trim: true, required: true},
+  location: {type: String, required: true},
+  starts: {type: Date, required: true},
+  ends: {type: Date, required: true},
+  eventDescription: {type: String, required: true},
+  organizerName: {type: String, required: true},
+  organizerDescription: {type: String, required: true},
+  eventType: {type: String, required: true},
+  eventTopic: {type: String, required: true},
+  ticketPrice: {type: Number, required: true}
+},{
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}  
+})
+
+schema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Event', schema);
